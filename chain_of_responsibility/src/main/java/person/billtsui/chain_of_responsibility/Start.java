e3941af3e1681672764859c802a864d889f09376
Bill Tsui <dhubilltsui@gmail.com>
2023-03-08 10:09:56 +0800

    init:初始化

---
+++ chain_of_responsibility/src/main/java/person/billtsui/chain_of_responsibility/Start.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package person.billtsui.chain_of_responsibility;
+
+/**
+ *
+ * @author k.cui
+ */
+public class Start {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
